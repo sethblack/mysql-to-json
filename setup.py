d8b12b88084e4f4f9894acd35e9c1bffb689d74b
Seth Black <sblack@sethserver.com>
2018-02-15 11:09:30 -0600

    fixed bad docs

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
 Connects to a MySQL database and exports selected data to JSON.
 
 ## Usage
-```
+
 mysql-to-json [-h] [-d DATABASE] [-H HOSTNAME] [-P PORT] [-u USER] [-p]
                      [-e QUERY]
 
@@ -50,7 +50,6 @@ optional arguments:
   -p, --password        Shh! It's a secret.
   -e QUERY, --query QUERY
                         Query to run.
-```
 
 ## Examples
 
@@ -60,24 +59,18 @@ All examples simple select all table information from `information_schema` and s
 
 This assumes we have full access to the mysql database from localhost.
 
-```
 $> mysql-to-json -e 'SELECT * FROM information_schema.tables' > tables.json
-```
 
 ### Medium Complexity
 
 This explicitly sets a user and asks for a password, while still connecting to localhost.
 
-```
 $> mysql-to-json -d mysql -u seth -p -e 'SELECT * FROM information_schema.tables' > tables.json
-```
 
 ### All The Things!
 
 This explicitly sets every command line option available.
 
-```
 $> mysql-to-json -h mydbserver.myhost.com -P 3306 -d mysql -u seth -p -e 'SELECT * FROM information_schema.tables' > tables.json
-```
 """
 )
